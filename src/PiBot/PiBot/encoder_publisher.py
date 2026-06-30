f61b52c37ac579379a1a04c8accdbe6948ee92ce
SoggyOtter <alexandere42@outlook.com>
2026-06-29 21:44:41 -0500

    close bus after reading

--- src/PiBot/PiBot/encoder_publisher.py
+++ src/PiBot/PiBot/encoder_publisher.py
@@ -20,6 +20,7 @@ def read_angle_L():
     raw_data = bus.read_i2c_block_data(AS5600_ADDR, ANGLE_REG, 2)
     angle = (raw_data[0] << 8) | raw_data[1]  # Combine MSB and LSB
     angle = angle & 0x0FFF  # Mask to 12 bits
+    bus.close()
     return (angle / 4096.0) * 360.0  # Convert to degrees
 
 
@@ -29,6 +30,7 @@ def read_angle_R():
     raw_data = bus.read_i2c_block_data(AS5600_ADDR, ANGLE_REG, 2)
     angle = (raw_data[0] << 8) | raw_data[1]  # Combine MSB and LSB
     angle = angle & 0x0FFF  # Mask to 12 bits
+    bus.close()
     return (angle / 4096.0) * 360.0  # Convert to degrees
 
 
